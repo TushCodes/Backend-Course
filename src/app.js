039631c07110e7c6056d4e4771f08ddcbe0bd2d3
TushCodes <42437750+TushCodes@users.noreply.github.com>
2025-06-09 07:40:20 +0000

    utils code discussion

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(express.static('public'));
+app.use(cookieParser());
+
+
+app.get('/', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
